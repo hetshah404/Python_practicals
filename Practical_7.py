1f4422f1ce4514a1a897e2611e6d145f32a6e9c3
hetshah404 <78745355+hetshah404@users.noreply.github.com>
2022-04-14 11:00:40 +0530

    Update Practical_7.py

--- Practical_7.py
+++ Practical_7.py
@@ -1,7 +1,7 @@
 # Name : Het Shah
 # ID : 20CE127
 # Aim :  Lapindrome is defined as a string which when split in the middle, gives two halves having the same characters and same frequency of each character. If there are odd number of characters in the string, we ignore the middle character and check for lapindrome. For example gaga is a lapindrome, since the two halves ga and ga have the same characters with same frequency. Also, abccab, rotor and xyzxy are a few examples of lapindromes. Note that abbaab is NOT a lapindrome. The two halves contain the same characters but their frequencies do not match. Your task is simple. Given a string, you need to tell if it is a lapindrome. 
-# GITHUB LINK : 
+# GITHUB LINK : https://github.com/hetshah404/Python_practicals.git
 n = int(input())
 
 
