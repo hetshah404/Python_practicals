f9f08db1d121361cca66ae975df265bf700397eb
hetshah404 <78745355+hetshah404@users.noreply.github.com>
2022-04-14 10:46:49 +0530

    Update Practical_5.py

--- Practical_5.py
+++ Practical_5.py
@@ -2,7 +2,7 @@
 # ID : 20CE127
 # Aim :  You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa. Sample Input: HackerRank.com presents "Pythonist 2".
 # Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".
-# GITHUB LINK : 
+# GITHUB LINK : https://github.com/hetshah404/Python_practicals.git
 def swap_case(s):
     num = ""
     for let in s:
